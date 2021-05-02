1cf606db34dba31bcb695153f5f54d7efe8ee549
uyildirim <uguryildirim1903@gmail.com>
2021-05-02 16:40:06 +0300

    #GO

---
+++ workBook2/src/workBook2/BookManager.java
@@ -0,0 +1,13 @@
+package workBook2;
+
+public class BookManager {
+	
+	public void addToBook(Book book) {
+		System.out.println(book.name+ " Kitabý Eklendi.");
+	}
+	public void like(Book book) {
+		System.out.println(book.name+ " Kitabý begendiniz.");
+	}
+	
+
+}
